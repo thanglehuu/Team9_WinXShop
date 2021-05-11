385c5353c1c7770ba49f4b8ce958d0e47c73d7bd
thang le <bilun4450528@gmail.com>
2021-05-11 10:09:46 +0700

    Add project files.

---
+++ Team9_winxshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Team9_winxshop.MvcApplication" Language="C#" %>
---
+++ Team9_winxshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team9_winxshop.Startup))]
+namespace Team9_winxshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
