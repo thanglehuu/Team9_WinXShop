f942155f7f09b9f13dec18554a06815b40f85248
thang le <bilun4450528@gmail.com>
2021-05-18 16:39:28 +0700

    Thêm sản phẩm Controller.

---
+++ Team9_winxshop.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Team9_winxshop.Controllers;
+
+namespace Team9_winxshop.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            var controller = new AccountController();
+            var result = controller.Login("/Account/Login") as ViewResult;
+            
+            Assert.IsNotNull(result);
+        }
+    }
+}
