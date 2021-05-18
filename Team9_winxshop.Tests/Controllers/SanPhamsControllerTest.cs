6c62e26a99a962163bbedf268956d491e99edd8a
thang le <bilun4450528@gmail.com>
2021-05-18 16:57:07 +0700

    Add unit test for SanPhamsController

---
+++ Team9_winxshop.Tests/Controllers/SanPhamsControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Team9_winxshop.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Team9_winxshop.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Team9_winxshop.Tests.Controllers
+{
+    [TestClass]
+    public class SanPhamsControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var controller = new SanPhamsController();
+
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as List<SanPham>;
+            Assert.IsNotNull(model);
+
+            var db = new CT25Team19Entities();
+            Assert.AreEqual(db.SanPhams.Count(), model.Count);
+        }
+    }
+}
