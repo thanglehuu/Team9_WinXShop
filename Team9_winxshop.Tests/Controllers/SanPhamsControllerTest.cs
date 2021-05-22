1e891b794b47dbcb7e7d53164d425863c82901d5
thang le <bilun4450528@gmail.com>
2021-05-22 12:25:33 +0700

    Add search products and unit test

--- Team9_winxshop.Tests/Controllers/SanPhamsControllerTest.cs
+++ Team9_winxshop.Tests/Controllers/SanPhamsControllerTest.cs
@@ -134,5 +134,76 @@ namespace Team9_winxshop.Tests.Controllers
             Assert.AreEqual(product.HinhAnh, model.HinhAnh);
             Assert.AreEqual(product.Size, model.Size);
         }
+
+        [TestMethod]
+        public void TestDispose()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestDetails()
+        {
+            var controller = new SanPhamsController();
+            var result0 = controller.Details("") as HttpNotFoundResult;
+            Assert.IsNotNull(result0);
+
+            var db = new CT25Team19Entities();
+            var product = db.SanPhams.First();
+            var result = controller.Details(product.MaSP) as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as SanPham;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(product.MaSP, model.MaSP);
+            Assert.AreEqual(product.TenSP, model.TenSP);
+            Assert.AreEqual(product.SoLuong, model.SoLuong);
+            Assert.AreEqual(product.MaLoaiSP, model.MaLoaiSP);
+            Assert.AreEqual(product.Mau, model.Mau);
+            Assert.AreEqual(product.HinhAnh, model.HinhAnh);
+            Assert.AreEqual(product.Size, model.Size);
+        }
+
+        [TestMethod]
+        public void TestSearch()
+        {
+            var db = new CT25Team19Entities();
+            var products = db.SanPhams.ToList();
+            var keyword = products.First().TenSP.Split().First();
+            products = products.Where(p => p.TenSP.ToLower().Contains(keyword.ToLower())).ToList();
+
+            var controller = new SanPhamsController();
+            var result = controller.Search(keyword) as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as List<SanPham>;
+            Assert.IsNotNull(model);
+
+            Assert.AreEqual("Index2", result.ViewName);
+            Assert.AreEqual(products.Count(), model.Count);
+            Assert.AreEqual(keyword, result.ViewData["keyword"]);
+        }
+
+        [TestMethod]
+        public void TestSearch2()
+        {
+            var db = new CT25Team19Entities();
+            var products = db.SanPhams.ToList();
+            var keyword1 = products.First().TenSP.Split().First();
+            var keyword2 = products.First().TenSP.Split().First();
+            products = products.Where(p => p.TenSP.ToLower().Contains(keyword1.ToLower()) || p.MaSP.ToLower().Contains(keyword2.ToLower())).ToList();
+
+            var controller = new SanPhamsController();
+            var result = controller.Search(keyword1) as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as List<SanPham>;
+            Assert.IsNotNull(model);
+
+            Assert.AreEqual("Index2", result.ViewName);
+            Assert.AreEqual(products.Count(), model.Count);
+            Assert.AreEqual(keyword1, result.ViewData["keyword"]);
+            Assert.AreEqual(keyword2, result.ViewData["keyword"]);
+        }
     }
 }
