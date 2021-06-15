38d8158d4148e449d43cca27243194eea451a335
197PM21984 - NGUYỄN TRỌNG THẮNG - K25T-PM1 <thang.197pm21984@vanlanguni.vn>
2021-06-15 21:17:06 +0700

    Add manage product,manage home, manage home

---
+++ Team9_winxshop/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Team9_winxshop.Models;
+
+namespace Team9_winxshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CT25Team19Entities db = new CT25Team19Entities();
+        public ActionResult Index()
+        {
+            var model = db.SanPhams.ToList();
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
